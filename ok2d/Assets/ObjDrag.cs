95443a369aac7c7c17b399207895246c51c4130a
matsubara_ke <matsubara_ke@micware.co.jp>
2025-02-14 12:56:05 +0900

    ok2d update

--- ok2d/Assets/ObjDrag.cs
+++ ok2d/Assets/ObjDrag.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Linq;
@@ -7,33 +5,25 @@ using UnityEngine.UI;
 
 public class ObjDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    private Vector2 prevPos; //保存しておく初期position
-    private RectTransform rectTransform; // 移動したいオブジェクトのRectTransform
-    private RectTransform parentRectTransform; // 移動したいオブジェクトの親(Panel)のRectTransform
     private GameObject dragobj;
-    private GameObject parent;
     private GameObject editScreen;
 
     private void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
-        parentRectTransform = rectTransform.parent as RectTransform;
-        parent = transform.parent.gameObject;
         editScreen = GameObject.Find("editScreen");
     }
 
     // ドラッグ開始時の処理
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // ドラッグ前の位置を記憶しておく
-        // RectTransformの場合はpositionではなくanchoredPositionを使う
-        // prevPos = rectTransform.anchoredPosition;
-        // Debug.Log("OnBegingDrag");
+        Debug.Log("OnBegingDrag");
 
         // オブジェクトを複製する
-        dragobj = Instantiate(gameObject, editScreen.GetComponent<RectTransform>()/* parentRectTransform*/);
-        // dragobj = Instantiate(gameObject, parentRectTransform);
-        dragobj.GetComponent<ObjDrag>().enabled = false; // 複製されたオブジェクトのドラッグハンドリングを無効にする
+        dragobj = Instantiate(gameObject, editScreen.GetComponent<RectTransform>());
+
+        // インスタンス時になぜかアンカーが変化するため、再設定
+        dragobj.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1);
+        dragobj.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1);
     }
 
     // ドラッグ中の処理
@@ -41,10 +31,14 @@ public class ObjDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragH
     {
         if (dragobj != null)
         {
-            Vector2 localPosition = GetLocalPosition(eventData.position);
             dragobj.transform.position = eventData.position;
         }
         // Debug.Log("OnDrag" + eventData.position + ", " + rectTransform.anchoredPosition);
+    }
+
+    Vector2 MultipleFloorPos(Vector2 value, Vector2 multiple)
+    {
+        return new Vector2(Mathf.Floor(value.x / multiple.x) * multiple.x, Mathf.Floor(value.y / multiple.y) * multiple.y);
     }
 
     // ドラッグ終了時の処理
@@ -54,53 +48,18 @@ public class ObjDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragH
         {
             // Destroy(dragobj); // 複製されたオブジェクトを破棄する
             RectTransform con = editScreen.transform.GetComponentsInChildren<RectTransform>(true).Where((_ => _.name == "Content")).FirstOrDefault();
-            dragobj.transform.parent = con;
-            Vector2 p = new Vector2(
-                eventData.position.x - editScreen.GetComponent<RectTransform>().position.x,
-                editScreen.GetComponent<RectTransform>().position.y - eventData.position.y);
-            Vector2 p2 = new Vector2(Mathf.Floor(p.x/128), Mathf.Floor(p.y/128));
-            Vector3 pos = dragobj.transform.position;
-            pos.x = p2.x*128+64/*+editScreen.transform.position.x*/;
-            pos.y = -(p2.y*128)-64/*+editScreen.transform.position.y*/;
-            Debug.Log(pos+"==="+editScreen.transform.position);
-            dragobj.transform.position = pos + editScreen.transform.position;  // 座標を設定
-
-            // Debug.Log(con);
-            
-
+            dragobj.transform.SetParent(con);
+            var objsize = dragobj.GetComponent<RectTransform>().sizeDelta;
+            dragobj.GetComponent<RectTransform>().anchoredPosition = MultipleFloorPos(dragobj.GetComponent<RectTransform>().anchoredPosition, objsize) + objsize/2;
 
             // editScreen サイズの調整
-            // Debug.Log("OnEndDrag>> "+editScreen.transform);
-            LayoutElement layoutElement = editScreen.transform.GetComponentsInChildren<LayoutElement>(true)[0];
-            Debug.Log(layoutElement.preferredWidth+"<-->"+pos.x+64);
-            if (layoutElement.preferredWidth < dragobj.transform.position.x+64) {
-                layoutElement.preferredWidth = dragobj.transform.position.x+64;
-                Debug.Log(layoutElement.preferredWidth+", "+pos.x);
+            if (con.sizeDelta.x < dragobj.GetComponent<RectTransform>().localPosition.x + objsize.x*2) {
+                con.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, dragobj.GetComponent<RectTransform>().localPosition.x + objsize.x*2);
             }
-            // layoutElement.Preferred Width
-            // var match = allTransform.Where((_ => _.name == "Content"));
-            // layoutElement = match.FirstOrDefault().transform.GetComponent<LayoutElement>();
-            // Transform con = editScreen.transform.Find("Content");
-            // GameObject con = editScreen.GetComponentsInChildren<Text>();
-            // editScreen.GetComponentInChild<Text>();
-            // editScreen.transform.Find("Scroll View").GetComponent<ScrollRect>();
-            // con.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1024);
-            // con.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1024);
-            // if (pos.y + 64 > editScreen.transform.size.x) {
-            //     Debug.Log("OnEndDrag");
-            // }
+            if (con.sizeDelta.y < Mathf.Abs(dragobj.GetComponent<RectTransform>().localPosition.y) + objsize.y*2) {
+                con.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Abs(dragobj.GetComponent<RectTransform>().localPosition.y) + objsize.y*2);
+            }
         }
         Debug.Log("OnEndDrag");
-    }
-
-    // ScreenPositionからlocalPositionへの変換関数
-    private Vector2 GetLocalPosition(Vector2 screenPosition)
-    {
-        Vector2 result = Vector2.zero;
-
-        // screenPositionを親の座標系(parentRectTransform)に対応するよう変換する.
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPosition, Camera.main, out result);
-
-        return result;
     }
 }
